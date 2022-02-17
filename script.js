e9c112fd3af5176ebfb510b1672d00544fab7c15
abugi <abugiboi@gmail.com>
2022-02-17 07:33:59 +0100

    basic types in javascript

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 /*
-    1. Typescript is a langauge built on javascript to give it
-    static typed capabilities. It also has other features which
-    are not present in Javascript.
-
-    2. You can write pure javscript code in typescript and it
-    will run without any issues. But the file carrying your
-    typescript code must end with ".ts" extension
-
-    3. Since the browser does not support typescript natively, all
-    typescript code must be be compiled down to javascript in order
-    to run in the browser.
-
-    4. Download and install the typescript compiler from npm using the command
-    "npm install -g typescript" (Note: you must have Node.js installed)
-
-    5. In your terminal, run tsc script.ts (assuming your filename is script.ts).
-    This will compile your typescript code and inject it into a file called
-    "script.js" in the same directory as the script.ts file
-
-    6. Finally, link your HTML file to the script.js file. You are all set!
+   Note:
+   1. the main difference between typescript and javascript is the type definition.
+    Typescript uses strict type and javascript uses dynamic types. this means that
+    when a variable is initialized in typescript with a certain datatype, it cannot
+    be changed later. e.g
 */
-var _name = 'Neemat';
-console.log(_name);
+var _name = 'Fatimah';
+var height = 5.4;
+var isMarried = true;
+/*
+   Looking at the exaxmples above...
+
+   1. variable _name can never be reassigned to another value that is not a string
+   2. height can be reassigned to a value that is a number datatype but never to a
+   different datatype
+   3. the value of isMarried must always be a Boolean
+*/
+// So the following will not work in typescript
+// _name = 2001 //number is not reassignable to a string
+// height = '5.4' // string is not reassignable to a number
+// isMarried = 'yes' // string is not reassignable to a boolean
+/*
+   Notice how we did not have to specify the types?
+
+   Well, thats because typescript uses 'inference'
+   In inference, typescript determines the type of a varibale based on the value
+   assigned to it.
+*/
+//Specifying type passed in to a function
+// const circ = (diameter) => {
+//    return diameter * Math.PI
+// }
+// At the moment we can pass anything to this function because not type is specified
+// console.log(circ('me'))
+// but the only meaningful data type for this function is a number
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circ('12')) // this is wrong
+console.log(circ(12));
