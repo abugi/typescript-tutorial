6e3ec752064998b4f9611bb5a94b220fba142ed6
abugi <abugiboi@gmail.com>
2022-02-18 21:43:01 +0100

    explicit types (primitives and arrays)

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
-// Array
-var list = ['macbook', 'iWatch', 'iPadPro'];
 /*
-   To typescript:
-   1. the variable 'list' can only be assigned the a value that is an array
-   2. the array can contain only string data types
+   We have previously mentioned that you don't have to specify the data type on a
+   variable because typescript does that automatically using inference.
+   Well this is only true if the said variable is initialized with a value.
+   But when a variable is not initialized, then type inference will not be
+   possible and this makes it possible to assign the variable any value in the
+   future which defeats the purpose of using typescript.
 */
-// So unlike javascript, the following will not work in typescript
-// list = 'macbook'
-// list.push(12)
-// list[0] = {name: 'teema'}
-// Instead
-list = ['1', '2', '3'];
-list.push('12');
-list[0] = '11';
-// Lets look at another example
-var mixed = ['one', 1, 'two', 2];
-/*
-   To typescript:
-   1. the variable 'list' can only be assigned the a value that is an array
-   2. the array contain a mixture of string and number data types only
+// Example
+var laptop;
+laptop = 'makbook pro';
+laptop = 'lenovo tinkpad';
+laptop = ['macbook pro', 780000, 'lenovo tink pad', 620000];
+console.log(laptop);
+/* To get around this, we can explicitly give the variable a type so that only
+that type can be assigned to it in the future
 */
-// So the following will be valid
-mixed = ['sam dede', 49];
-mixed.push('samson noah', 46);
-// and this will be invalid
-// mixed = ['age', 12, false]
-// mixed.push(['next'])
-console.log(list);
-console.log(mixed);
-// Object
-var wife = {
-    name: 'Neematallah',
-    age: 27,
-    isCute: true
-};
+// Example
+var device; // now only string values can be assing to this variable
+var quantity; // only number can be assigned in the future
+var isAvailable; // only boolean values can be assigned
+// to initialize these variables
+device = 'macbook pro 2022'; // correct
+// device = 2022 // wrong
+quantity = 2; // correct
+// quantity = '2'
+isAvailable = true; // correct ✔
+// isAvailable = 'true' // wrong ❌
+console.log(device);
+console.log(quantity);
+console.log(isAvailable);
+// Explicit type in Arrays
+//   To declare a variable that only accepts an array of strings
+var devices;
+devices = ['macbook pro', 'airpods pro']; // correct ✓
+// devices = [1,2,3] // wrong ❌
+console.log(devices);
 /*
-   Considering the object above, the following rules apply in typescript
+   Note: Array methods like .push(), .splice() etc. will not work on
+   devides array because the variable has not been initialized
+*/
+// Declare an array that only accept numbers and initialize with empty array
+var ages = [];
+ages = [19, 20, 21, 22];
+ages.push(25);
+ages.shift();
+console.log(ages);
+// Union types
+/*
+   this allows us to declare a variable that stores an array of mixed data types.
 
-   1. the variable wife can only be assigned an object
-   2. any object assigned to the variable must have the same structure i.e
-    the same of keys and same value types
-   3. once an object is defined, we cannot add additional properties to it
-   4. the existing properties of the object can be changed with a value of same type
 */
-// So the following will not work
-// wife = 'Neematallahi'
-// wife = {
-//    name: 'Neemat',
-//    age: '27',
-//    isCute: 'false'
-// }
-// wife.color = 'black'
-// the following will be valid
-wife.age = 19;
-wife.name = 'Neemat';
-console.log(wife);
+var mixed = []; // this variable will store string and number types
+mixed = ['musa abugi', 30, 'black hair', 6.7]; // correct ✔️
+// mixed = ['musa abugi', 30, true, ['abugi']] // wrong ❌
+// mixed.push(false) // wrong ❌
+// Union type on primitives
+var uid; // the value of uid can either be a string or number
+uid = '123456'; // correct ✔️
+uid = 123456; // correct ✔️
+// uid = false // wrong ❌
+console.log(uid);
+// Explicit type in Objects
--- script.ts
+++ script.ts
@@ -1,76 +1,93 @@
-// Array
-let list = ['macbook', 'iWatch', 'iPadPro']
-
 /*
-   To typescript:
-   1. the variable 'list' can only be assigned the a value that is an array
-   2. the array can contain only string data types
+   We have previously mentioned that you don't have to specify the data type on a
+   variable because typescript does that automatically using inference. 
+   Well this is only true if the said variable is initialized with a value.
+   But when a variable is not initialized, then type inference will not be
+   possible and this makes it possible to assign the variable any value in the
+   future which defeats the purpose of using typescript.
 */
 
-// So unlike javascript, the following will not work in typescript
+// Example
+let laptop
 
-// list = 'macbook'
-// list.push(12)
-// list[0] = {name: 'teema'}
+laptop = 'makbook pro'
 
-// Instead
-list = ['1','2','3']
-list.push('12')
-list[0] = '11'
+laptop = 'lenovo tinkpad'
+
+laptop = ['macbook pro', 780000, 'lenovo tink pad', 620000]
+
+console.log(laptop)
 
 
-// Lets look at another example
-let mixed = ['one', 1, 'two', 2]
-
-/*
-   To typescript:
-   1. the variable 'list' can only be assigned the a value that is an array
-   2. the array contain a mixture of string and number data types only
+/* To get around this, we can explicitly give the variable a type so that only
+that type can be assigned to it in the future
 */
 
-// So the following will be valid
-mixed = ['sam dede', 49]
-mixed.push('samson noah', 46)
-
-// and this will be invalid
-// mixed = ['age', 12, false]
-// mixed.push(['next'])
-
-console.log(list)
-console.log(mixed)
+// Example
+let device: string // now only string values can be assing to this variable
+let quantity: number // only number can be assigned in the future
+let isAvailable: boolean // only boolean values can be assigned
 
 
-// Object
-let wife = {
-   name: 'Neematallah',
-   age: 27,
-   isCute: true
-}
+// to initialize these variables
+device = 'macbook pro 2022' // correct
+// device = 2022 // wrong
+
+quantity = 2 // correct
+// quantity = '2'
+
+isAvailable = true // correct ✔
+// isAvailable = 'true' // wrong ❌
+
+console.log(device)
+console.log(quantity)
+console.log(isAvailable)
+
+
+// Explicit type in Arrays
+
+//   To declare a variable that only accepts an array of strings
+let devices: string[]
+devices = ['macbook pro', 'airpods pro'] // correct ✓
+
+// devices = [1,2,3] // wrong ❌
+
+
+console.log(devices)
 
 /*
-   Considering the object above, the following rules apply in typescript
-
-   1. the variable wife can only be assigned an object
-   2. any object assigned to the variable must have the same structure i.e
-    the same of keys and same value types
-   3. once an object is defined, we cannot add additional properties to it
-   4. the existing properties of the object can be changed with a value of same type
+   Note: Array methods like .push(), .splice() etc. will not work on
+   devides array because the variable has not been initialized
 */
 
-// So the following will not work
+// Declare an array that only accept numbers and initialize with empty array
+let ages: number[] = []
+ages = [19, 20, 21, 22]
+ages.push(25)
+ages.shift()
 
-// wife = 'Neematallahi'
+console.log(ages)
 
-// wife = {
-//    name: 'Neemat',
-//    age: '27',
-//    isCute: 'false'
-// }
+// Union types
+/*
+   this allows us to declare a variable that stores an array of mixed data types.
 
-// wife.color = 'black'
+*/
 
-// the following will be valid
-wife.age = 19
-wife.name = 'Neemat'
+let mixed:(string|number)[] = [] // this variable will store string and number types
+mixed = ['musa abugi', 30, 'black hair', 6.7] // correct ✔️
 
-console.log(wife)
+// mixed = ['musa abugi', 30, true, ['abugi']] // wrong ❌
+// mixed.push(false) // wrong ❌
+
+// Union type on primitives
+let uid: string|number  // the value of uid can either be a string or number
+
+uid = '123456' // correct ✔️
+uid = 123456 // correct ✔️
+// uid = false // wrong ❌
+
+console.log(uid)
+
+
+// Explicit type in Objects
