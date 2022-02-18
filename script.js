775a339d753e82ab2c2978b55c5a32334ce36ba4
abugi <abugiboi@gmail.com>
2022-02-18 22:45:31 +0100

    typescript any

--- script.js
+++ script.js
@@ -1,88 +1,33 @@
 /*
-   We have previously mentioned that you don't have to specify the data type on a
-   variable because typescript does that automatically using inference.
-   Well this is only true if the said variable is initialized with a value.
-   But when a variable is not initialized, then type inference will not be
-   possible and this makes it possible to assign the variable any value in the
-   future which defeats the purpose of using typescript.
+ 1. i typescript, 'any' is used to declare a variable to accept any type
+   of data in the future
+2. inference does not apply here which means the variable can be reassigned
+   any type of data even after it is initialized
+3. this is like reversing typescript to work just like javascript.
 */
-// Example
-var laptop;
-laptop = 'makbook pro';
-laptop = 'lenovo tinkpad';
-laptop = ['macbook pro', 780000, 'lenovo tink pad', 620000];
-console.log(laptop);
-/* To get around this, we can explicitly give the variable a type so that only
-that type can be assigned to it in the future
-*/
-// Example
-var device; // now only string values can be assing to this variable
-var quantity; // only number can be assigned in the future
-var isAvailable; // only boolean values can be assigned
-// to initialize these variables
-device = 'macbook pro 2022'; // correct
-// device = 2022 // wrong
-quantity = 2; // correct
-// quantity = '2'
-isAvailable = true; // correct ✔
-// isAvailable = 'true' // wrong ❌
-console.log(device);
-console.log(quantity);
-console.log(isAvailable);
-// Explicit type in Arrays
-//   To declare a variable that only accepts an array of strings
-var devices;
-devices = ['macbook pro', 'airpods pro']; // correct ✓
-// devices = [1,2,3] // wrong ❌
-console.log(devices);
-/*
-   Note: Array methods like .push(), .splice() etc. will not work on
-   devides array because the variable has not been initialized
-*/
-// Declare an array that only accept numbers and initialize with empty array
-var ages = [];
-ages = [19, 20, 21, 22];
-ages.push(25);
-ages.shift();
-console.log(ages);
-// Union types
-/*
-   this allows us to declare a variable that stores an array of mixed data types.
-
-*/
-var mixed = []; // this variable will store string and number types
-mixed = ['musa abugi', 30, 'black hair', 6.7]; // correct ✔️
-// mixed = ['musa abugi', 30, true, ['abugi']] // wrong ❌
-// mixed.push(false) // wrong ❌
-// Union type on primitives
-var uid; // the value of uid can either be a string or number
-uid = '123456'; // correct ✔️
-uid = 123456; // correct ✔️
-// uid = false // wrong ❌
-console.log(uid);
-// Explicit type in Objects
-var object1; // object1 can only hold values which are objects
 // Examples
-object1 = {
-    name: 'peter rabbit',
-    occupation: 'actor'
-};
-object1 = []; // this is also correct because Array is a form of object 
-object1 = function () { }; // this is also correct because is an object type
-// object1 = 12 // wrong ❌
-console.log(object1);
-// If you wnat to be more specific on the type object
-var object2; // now only actual objects can be store in this variable and must have the same structure and defined data types
-object2 = {
-    name: 'umar bn affan',
-    role: 'caliph',
-    age: 71,
-    isAlive: false
-};
-// object2 = {
-//    name: 'yahaya haifan',
-//    role: 'da\'i',
-//    age: '70',
-//    isAlive: true
-// }  // this is not correct because the age property must be a number and not a string
-console.log(object2);
+var gullible = 'billi';
+console.log(gullible);
+gullible = 1769;
+console.log(gullible);
+// applying to arrays
+var thundermans = []; // thundermans must be an array but can contain any type
+thundermans = ['max', 'nora', 'chloe'];
+thundermans.push({ dad: 'hank', mum: 'electress' });
+thundermans.push(true);
+thundermans.unshift('phoebe');
+console.log(thundermans);
+// applying to objects
+var cast; // cast must an object where name ppty is a string feature can be any type and episodes can be any type
+// Examples
+cast = {
+    name: 'henry',
+    featured: true,
+    episodes: 'all'
+}; // correct
+// cast = {
+//    name: true,
+//    featured: false,
+//    episodes: 1
+// } // wrong ❌ because name must be a string type
+console.log(cast);
--- script.ts
+++ script.ts
@@ -1,132 +1,52 @@
 /*
-   We have previously mentioned that you don't have to specify the data type on a
-   variable because typescript does that automatically using inference. 
-   Well this is only true if the said variable is initialized with a value.
-   But when a variable is not initialized, then type inference will not be
-   possible and this makes it possible to assign the variable any value in the
-   future which defeats the purpose of using typescript.
+ 1. i typescript, 'any' is used to declare a variable to accept any type
+   of data in the future
+2. inference does not apply here which means the variable can be reassigned
+   any type of data even after it is initialized
+3. this is like reversing typescript to work just like javascript.
 */
-
-// Example
-let laptop
-
-laptop = 'makbook pro'
-
-laptop = 'lenovo tinkpad'
-
-laptop = ['macbook pro', 780000, 'lenovo tink pad', 620000]
-
-console.log(laptop)
-
-
-/* To get around this, we can explicitly give the variable a type so that only
-that type can be assigned to it in the future
-*/
-
-// Example
-let device: string // now only string values can be assing to this variable
-let quantity: number // only number can be assigned in the future
-let isAvailable: boolean // only boolean values can be assigned
-
-
-// to initialize these variables
-device = 'macbook pro 2022' // correct
-// device = 2022 // wrong
-
-quantity = 2 // correct
-// quantity = '2'
-
-isAvailable = true // correct ✔
-// isAvailable = 'true' // wrong ❌
-
-console.log(device)
-console.log(quantity)
-console.log(isAvailable)
-
-
-// Explicit type in Arrays
-
-//   To declare a variable that only accepts an array of strings
-let devices: string[]
-devices = ['macbook pro', 'airpods pro'] // correct ✓
-
-// devices = [1,2,3] // wrong ❌
-
-
-console.log(devices)
-
-/*
-   Note: Array methods like .push(), .splice() etc. will not work on
-   devides array because the variable has not been initialized
-*/
-
-// Declare an array that only accept numbers and initialize with empty array
-let ages: number[] = []
-ages = [19, 20, 21, 22]
-ages.push(25)
-ages.shift()
-
-console.log(ages)
-
-// Union types
-/*
-   this allows us to declare a variable that stores an array of mixed data types.
-
-*/
-
-let mixed:(string|number)[] = [] // this variable will store string and number types
-mixed = ['musa abugi', 30, 'black hair', 6.7] // correct ✔️
-
-// mixed = ['musa abugi', 30, true, ['abugi']] // wrong ❌
-// mixed.push(false) // wrong ❌
-
-// Union type on primitives
-let uid: string|number  // the value of uid can either be a string or number
-
-uid = '123456' // correct ✔️
-uid = 123456 // correct ✔️
-// uid = false // wrong ❌
-
-console.log(uid)
-
-
-// Explicit type in Objects
-let object1: object // object1 can only hold values which are objects
 
 // Examples
-object1 = {             // correct
-name: 'peter rabbit',
-occupation: 'actor'
-}
+let gullible: any = 'billi'
 
-object1 = [] // this is also correct because Array is a form of object 
+console.log(gullible)
 
-object1 = function(){} // this is also correct because is an object type
+gullible = 1769
 
-// object1 = 12 // wrong ❌
+console.log(gullible)
 
-console.log(object1)
+// applying to arrays
+let thundermans: any[] = [] // thundermans must be an array but can contain any type
 
-// If you wnat to be more specific on the type object
-let object2: {
-   name: string,
-   role: string,
-   age: number,
-   isAlive: boolean
-} // now only actual objects can be store in this variable and must have the same structure and defined data types
+thundermans = ['max', 'nora', 'chloe']
 
-object2 = {
-   name: 'umar bn affan',
-   role: 'caliph',
-   age: 71,
-   isAlive: false
-}
+thundermans.push({dad: 'hank', mum: 'electress'})
 
-// object2 = {
-//    name: 'yahaya haifan',
-//    role: 'da\'i',
-//    age: '70',
-//    isAlive: true
-// }  // this is not correct because the age property must be a number and not a string
+thundermans.push(true)
 
-console.log(object2)
+thundermans.unshift('phoebe')
+
+console.log(thundermans)
+
+
+// applying to objects
+let cast: {
+   name: string, 
+   featured: any, 
+   episodes: any
+} // cast must an object where name ppty is a string feature can be any type and episodes can be any type
+
+// Examples
+cast = {
+   name: 'henry',
+   featured: true,
+   episodes: 'all'
+} // correct
+
+// cast = {
+//    name: true,
+//    featured: false,
+//    episodes: 1
+// } // wrong ❌ because name must be a string type
+
+console.log(cast)
