7a06ba66d2ac6ad96285842d18d6a11060c3dcde
abugi <abugiboi@gmail.com>
2022-02-24 23:58:15 +0100

    type aliases

--- public/script.js
+++ public/script.js
@@ -1,53 +1,25 @@
 "use strict";
-let func = () => console.log('this is a funciton');
 /*
-    1. typescript automatically gives the variable func a type Function
-    due to type inference as it applies to string and numbers
-    2. assigning a value of another type will not be allowed
+  1. if you have functions that use similar types or similar group of types (in the
+    case of union types), it can get very repeatitive and cumbersome.
+  2. typescript solves this kind of problems using "type aliases" to reduce code
+  duplication
+  3. define it once, use it anywhere and as many times as you want
 */
 // Example
-// func = 'this is illegal' // wrong ❌
-// Explicit function type
-let greet; // only function ca be assigned to greet
-greet = () => 'Hello there!'; // correct
-// greet = {name: 'musa abugi'} // wrong ❌
-// Implicit function parameter type
-const add = (param1, param2) => {
-    console.log(param1 + param2);
-}; // only numbers can be passed into the function as parameters
-// Examples
-add(12, 8); // correct
-// add(7, '23') // wrong ❌
-// Passing optional parameters
-const minus = (x, y, z) => {
-    console.log(x - y);
-    console.log(z);
+const longDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-/*
- 1. adding a question mark in front of the 3rd parameter makes it optional
- 2. the 3rd parameter is a union type of either a string or a number
- 3. when the 3rd parameter is not provided, the funciton returns undefined for it
-*/
-minus(56, 8); // z = undefined
-// the return key word
-const multiply = (a, b) => {
-    return a * b;
+longDetails(123456, 'plunger');
+const greet = (user) => {
+    console.log(`${user.name} with ${user.uid} says Hello!`);
 };
-/*
-    1. when a function returns a value, the type of that funciton is the
-    value it returns (inference)
-*/
-let result = multiply(12, 13);
-result = 23; // correct
-// result = '23'
-console.log(result);
-/*
-    When a funtion does not explicitly return  a value,
-    the return type for such funtion is "void"
-*/
-// Example
-const devide = function (a, b) {
-    console.log(a / b);
+console.log(greet({ name: 'musa abugi', uid: 123456 }));
+const longDetails2 = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-console.log(devide(12, 3));
-// the type of devide is void
+longDetails2(123456, 'quidward');
+const greet2 = (user) => {
+    console.log(`${user.name} with ${user.uid} says Hello!`);
+};
+console.log(greet2({ name: 'jonjo shelvey', uid: 123456 }));
